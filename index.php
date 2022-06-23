7813cee650b3d53f01b9d6e72b3ee8ed2f2d6897
Sonya <91089885+devilmah@users.noreply.github.com>
2022-06-23 21:56:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 <?php
 echo "<body style='background-image: url(gr.jpg);background-size: cover;'>";
-include('bd.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   if (!empty($_GET['save'])) {
@@ -44,6 +43,8 @@ if ($errors) {
 		<h2 class='text-center'>" . $error_text . "</h2></div></div>";
   exit();
 }
+
+include('bd.php');
 
 try {
   $stmt = $db->prepare("INSERT INTO users (firstname, email, bdate, sex, amount, immortality, god, fly, bio) VALUES (:firstname, :email, :bdate, :sex, :amount, :immortality, :god, :fly, :bio)");
